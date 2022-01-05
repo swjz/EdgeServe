961dd8886cca8b080aa2c35e40e30c4068a868ed
Ted Shaowang <tedswjz@gmail.com>
2022-01-05 13:27:35 -0600

    more test

--- edgeserve/test_pipeline.py
+++ edgeserve/test_pipeline.py
@@ -5,13 +5,30 @@ from materialize import Materialize
 
 class TestPipeline():
     def test_pipeline(self):
-        node = 'pulsar://swjz-nuc2:6650'
-        s = 'hello'
+        node = 'pulsar://localhost:6650'
+        stream = 'hello'
         task = lambda x: x*2
-        with DataSource(stream=s, pulsar_node=node) as data_source,\
-                Compute(task=task, pulsar_node=node) as compute,\
-                Materialize(lambda x: x, pulsar_node=node) as materialize:
-            for l in s:
+        with DataSource(stream, node) as data_source,\
+                Compute(task, node) as compute,\
+                Materialize(lambda x: x, node) as materialize:
+            for l in stream:
                 assert next(data_source) == l.encode('utf-8')
                 assert next(compute) == task(l).encode('utf-8')
+                assert next(materialize) == task(l)
+
+
+    def test_pipeline_separate(self):
+        node = 'pulsar://localhost:6650'
+        stream = 'hello'
+        task = lambda x: x*2
+        with DataSource(stream, node) as data_source:
+            for l in stream:
+                assert next(data_source) == l.encode('utf-8')
+
+        with Compute(task, node) as compute:
+            for l in stream:
+                assert next(compute) == task(l).encode('utf-8')
+
+        with Materialize(lambda x: x, node) as materialize:
+            for l in stream:
                 assert next(materialize) == task(l)
