4b55ce1abd4bdd4e7527dc4822f63900786b195b
Ted Shaowang <tedswjz@gmail.com>
2022-02-02 03:18:15 -0600

    add worker interface

---
+++ edgeserve/worker.py
@@ -0,0 +1,57 @@
+import sys
+import pulsar
+from edgeserve.util import ftp_fetch
+from io import StringIO
+from contextlib import redirect_stdout, redirect_stderr
+
+
+class Worker:
+    def __init__(self, pulsar_node, topic='code-topic', ftp=False, ftp_memory=True, local_ftp_path='/srv/ftp/'):
+        self.client = pulsar.Client(pulsar_node)
+        self.consumer = self.client.subscribe(topic, subscription_name='my-sub')
+        self.gate = lambda x: x.decode('utf-8')
+        self.ftp = ftp
+        self.local_ftp_path = local_ftp_path
+        self.ftp_memory = ftp_memory
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.client.close()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        msg = self.consumer.receive()
+        data = self.gate(msg.data())
+
+        if self.ftp and not self.ftp_memory:  # FTP file mode
+            # download the file from FTP server and then delete the file from server
+            local_file_path = ftp_fetch(data, self.local_ftp_path, memory=False, delete=False)
+            data = open(local_file_path).read()
+        else:
+            if self.ftp:  # FTP memory mode
+                data = ftp_fetch(data, self.local_ftp_path, memory=True, delete=False)
+
+        stdout, stderr = StringIO(), StringIO()
+        with redirect_stdout(stdout), redirect_stderr(stderr):
+            exec(data)
+
+        print(stdout.getvalue())
+        print(stderr.getvalue())
+
+        self.consumer.acknowledge(msg)
+
+
+if __name__ == "__main__":
+    # Example: python worker.py pulsar://localhost:6650 code-ftp true false /srv/ftp/
+    node = sys.argv[1]
+    topic = sys.argv[2] if len(sys.argv) >= 3 else 'code-topic'
+    ftp = sys.argv[3] in ['true', 'True'] if len(sys.argv) >= 4 else False
+    ftp_memory = sys.argv[4] in ['true', 'True'] if len(sys.argv) >= 5 else True
+    local_ftp_path = sys.argv[5] if len(sys.argv) >= 6 else '/srv/ftp/'
+    with Worker(node, topic, ftp, ftp_memory, local_ftp_path) as worker:
+        while True:
+            next(worker)
---
+++ tests/test_worker.py
@@ -0,0 +1,64 @@
+from edgeserve.data_source import DataSource
+from edgeserve.worker import Worker
+import pytest
+
+
+data_source_code = """
+from data_source import DataSource
+node = 'pulsar://localhost:6650'
+stream = ['Hello World!']
+with DataSource(stream, node) as data_source:
+    next(data_source)
+"""
+
+compute_code = """
+from edgeserve.compute import Compute
+node = 'pulsar://localhost:6650'
+task = lambda x: x
+with Compute(task, node) as compute:
+    assert next(compute) == b'Hello World!'
+"""
+
+
+@pytest.fixture()
+def raw_data():
+    return {'node': 'pulsar://localhost:6650'}
+
+
+def test_worker(raw_data):
+    with DataSource([data_source_code, compute_code], raw_data['node'], topic='code-raw') as data_source:
+        next(data_source)
+        next(data_source)
+
+
+@pytest.fixture()
+def ftp_data():
+    return {'node': 'pulsar://localhost:6650',
+            'data_source_local': '/srv/ftp/files/data_source_code.py',
+            'compute_local': '/srv/ftp/files/compute_code.py',
+            'data_source_global': 'ftp://localhost/files/data_source_code.py',
+            'compute_global': 'ftp://localhost/files/compute_code.py'}
+
+
+def test_worker_ftp(ftp_data):
+    with open(ftp_data['data_source_local'], 'w') as f:
+        f.write(data_source_code)
+    with open(ftp_data['compute_local'], 'w') as f:
+        f.write(compute_code)
+
+    with DataSource([ftp_data['data_source_global'], ftp_data['compute_global']], ftp_data['node'],
+                    topic='code-ftp') as data_source:
+        next(data_source)
+        next(data_source)
+
+
+def test_worker_ftp_memory(ftp_data):
+    with open(ftp_data['data_source_local'], 'w') as f:
+        f.write(data_source_code)
+    with open(ftp_data['compute_local'], 'w') as f:
+        f.write(compute_code)
+
+    with DataSource([ftp_data['data_source_global'], ftp_data['compute_global']], ftp_data['node'],
+                    topic='code-ftp-memory') as data_source:
+        next(data_source)
+        next(data_source)
